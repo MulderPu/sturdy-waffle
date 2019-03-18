363623d0e37b59ab95ff6c8325adbe57f905d20b
Mulder Pu <mulderpu@gmail.com>
2019-03-18 23:00:05 +0800

    First Hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Message msg = new Message("Hello World");
+            msg.printMessage();
+            // prevent exit
+            Console.ReadLine();
+        }
+
+
+    }
+
+    public class Message
+    {
+
+        // variables
+        private string text;
+
+        public Message(string txt)
+        {
+            text = txt;
+        }
+
+        public void printMessage()
+        {
+            Console.WriteLine(text);
+        }
+
+
+    }
+}
