aa3725e45b64cd19d1298e93346e28dac3893df1
Mulder Pu <mulderpu@gmail.com>
2019-03-19 21:23:57 +0800

    Learnt Loop and If-else

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,8 +11,29 @@ namespace HelloWorld
 
         static void Main(string[] args)
         {
-            Message msg = new Message("Hello World");
-            msg.printMessage();
+            bool flag = true;
+            Message msg = new Message("Hello World! \nWelcome Back!");
+            msg.PrintMessage();
+
+            while (flag)
+            {
+                // receive input
+                Console.WriteLine("Enter your name?");
+                string input = Console.ReadLine();
+                //Console.WriteLine("Your name is {0}", input);
+
+                if (input != "exit")
+                {
+                    PascalCase newPascalCase = new PascalCase(input.ToLower());
+                    Console.WriteLine("-----------------------------------------");
+                }
+                else {
+                    flag = false;
+                    msg.SetMessage("Ok, Bye!");
+                    msg.PrintMessage();
+                }
+            }
+
             // prevent exit
             Console.ReadLine();
         }
@@ -31,11 +52,40 @@ namespace HelloWorld
             text = txt;
         }
 
-        public void printMessage()
+        public void PrintMessage()
         {
             Console.WriteLine(text);
         }
 
+        public void SetMessage(string _text) {
+            text = _text;
+        }
+    }
 
+    public class PascalCase
+    {
+        public PascalCase(string _text)
+        {
+
+            string reply = "Who are you?";
+            switch (_text)
+            {
+                case "james":
+                    reply = "Oh, You are back!";
+                    break;
+                case "alex":
+                    reply = "Long time no see!";
+                    break;
+                case "sharon":
+                    reply = "Beautiful, are you single?";
+                    break;
+                case "bryan":
+                    reply = "Fat as always.";
+                    break;
+            }
+
+            Message newMessage = new Message(reply);
+            newMessage.PrintMessage();
+        }
     }
 }
